82ae77f9c989ac29238aaab8ff54bdb4851a04f4
divykirtisrivastava <divykirtisrivastava@gmail.com>
2024-07-29 12:40:23 +0530

    add completed

--- frontend/src/adminPannel/AddProduct.jsx
+++ frontend/src/adminPannel/AddProduct.jsx
@@ -1,113 +1,121 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ArrowRight } from 'lucide-react'
+import axios from 'axios'
+import {useNavigate} from 'react-router-dom'
 
 export default function AddProduct() {
+
+  let navigation =  useNavigate()
+
+  let [data, sedtData] = useState({
+    productBrand: "",
+    productType: "",
+    productPrice: "",
+    productRating: "",
+  })
+  let {productBrand, productType, productPrice, productRating} = data
+
+  function handleChange(e){
+    sedtData({...data, [e.target.name]: e.target.value})
+  }
+
+async function handleSubmit(e){
+  e.preventDefault()
+
+  let result = await axios.post('http://localhost:3000/api/productSave', data)
+ navigation('/')
+}
+
   return (
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-2">
         <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
           <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
-            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">Sign up</h2>
-            <p className="mt-2 text-base text-gray-600">
-              Already have an account?{' '}
-              <a
-                href="#"
-                title=""
-                className="font-medium text-black transition-all duration-200 hover:underline"
-              >
-                Sign In
-              </a>
-            </p>
-            <form action="#" method="POST" className="mt-8">
+            <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">Add Product</h2>
+            
+            <form action="#" method="POST" className="mt-8" onSubmit={handleSubmit}>
               <div className="space-y-5">
                 <div>
                   <label htmlFor="name" className="text-base font-medium text-gray-900">
                     {' '}
-                    Full Name{' '}
+                   Product Brand{' '}
                   </label>
                   <div className="mt-2">
                     <input
                       className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                       type="text"
-                      placeholder="Full Name"
+                      placeholder="Product Brand"
+                      name='productBrand'
+                      value={productBrand}
+                    onChange={handleChange}
                       id="name"
                     ></input>
                   </div>
                 </div>
                 <div>
-                  <label htmlFor="email" className="text-base font-medium text-gray-900">
+                  <label htmlFor="name" className="text-base font-medium text-gray-900">
                     {' '}
-                    Email address{' '}
+                   Product Type{' '}
                   </label>
                   <div className="mt-2">
                     <input
                       className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                      type="email"
-                      placeholder="Email"
-                      id="email"
+                      type="text"
+                      placeholder="Product Brand"
+                      name='productType'
+                      value={productType}
+                    onChange={handleChange}
+                      id="name"
                     ></input>
                   </div>
                 </div>
                 <div>
-                  <div className="flex items-center justify-between">
-                    <label htmlFor="password" className="text-base font-medium text-gray-900">
-                      {' '}
-                      Password{' '}
-                    </label>
-                  </div>
+                  <label htmlFor="name" className="text-base font-medium text-gray-900">
+                    {' '}
+                   Product Price{' '}
+                  </label>
                   <div className="mt-2">
                     <input
                       className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                      type="password"
-                      placeholder="Password"
-                      id="password"
+                      type="text"
+                      placeholder="Product Price"
+                      name='productPrice'
+                      value={productPrice}
+                    onChange={handleChange}
+                      id="name"
                     ></input>
                   </div>
                 </div>
                 <div>
+                  <label htmlFor="name" className="text-base font-medium text-gray-900">
+                    {' '}
+                   Product Rating{' '}
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                      type="text"
+                      placeholder="Product Rating"
+                      name='productRating'
+                      value={productRating}
+                    onChange={handleChange}
+                      id="name"
+                    ></input>
+                  </div>
+                </div>
+             
+             
+                <div>
                   <button
-                    type="button"
+                    type="submit"
                     className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
                   >
-                    Create Account <ArrowRight className="ml-2" size={16} />
+                   Add Data <ArrowRight className="ml-2" size={16} />
                   </button>
                 </div>
               </div>
             </form>
-            <div className="mt-3 space-y-3">
-              <button
-                type="button"
-                className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
-              >
-                <span className="mr-2 inline-block">
-                  <svg
-                    className="h-6 w-6 text-rose-500"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                  >
-                    <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
-                  </svg>
-                </span>
-                Sign up with Google
-              </button>
-              <button
-                type="button"
-                className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
-              >
-                <span className="mr-2 inline-block">
-                  <svg
-                    className="h-6 w-6 text-[#2563EB]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                  >
-                    <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
-                  </svg>
-                </span>
-                Sign up with Facebook
-              </button>
-            </div>
+        
           </div>
         </div>
         <div className="h-full w-full">
